35e0c79b82edeef39dac136f68aacdd907f42d12
William Jackson <wcjaackson@gmail.com>
2025-06-29 13:45:56 +1000

    remove styling

--- main.py
+++ main.py
@@ -6,13 +6,5 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template('basepage.html')
 
-@app.route('/about')
-def about():
-    return flask.render_template('basepage.html')
-
-@app.route('/_/')
-def mockup():
-    return flask.render_template('mockup.html')
-
 if __name__ == "__main__":
     app.run(debug=True)
